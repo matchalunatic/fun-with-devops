2b4eb503cae493c494910ce3f01d593ea79e42da
MatchaLunatic <matcha@dave.null>
2019-08-07 12:01:46 +0200

    Fixed provisioner

---
+++ vm-images/docker-swarm/join-swarm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+INSTANCE_ID=`wget -qO- http://instance-data/latest/meta-data/instance-id`
+REGION=`wget -qO- http://instance-data/latest/meta-data/placement/availability-zone | sed 's/.$//'`
+aws ec2 describe-tags --region $REGION --filter "Name=resource-id,Values=$INSTANCE_ID" --output=text | sed -r 's/TAGS\t(.*)\t.*\t.*\t(.*)/\1="\2"/' > /etc/ec2-tags
+
+source /etc/ec2-tags
+
+export NODE_TYPE="${swarm_node_type}"
+export DYNAMODB_TABLE="swarm_locking_manager"
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker -v /var/log:/var/log srikalyan/aws-swarm-init
